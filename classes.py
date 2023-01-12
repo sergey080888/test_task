1998c36ddadac1ab7eae27774425297d29acb019
Сергей Ковальский <237264@mail.ru>
2023-01-12 22:03:58 +0300

    добавлено threding ParserRed

---
+++ classes.py
@@ -0,0 +1,240 @@
+import threading
+import os
+import requests
+import json
+import bs4
+
+
+class ParserRed:
+    super_count = 0
+    countries_dict = {}
+    big_wanted_list = []
+    super_param = []
+    param_list = []
+
+    def __init__(self, url, headers):
+        self.url = url
+        self.headers = headers
+        self.get_total()
+        self.making_country_dict()
+        self.param_maker()
+        self.make_dir()
+        self.make_json()
+
+    def text_parsing(self):
+        response = requests.get(self.url, self.headers)
+        text = response.text
+        soup = bs4.BeautifulSoup(text, features="html.parser")
+        print('text_parsing')
+        return soup
+
+    def get_total(self):
+        url = "https://ws-public.interpol.int/notices/v1/red"
+        response = requests.get(url, headers=self.headers)
+        print(f'Необходимо загрузить {response.json()["total"]}')
+        return
+
+   # словарь со странами
+    def making_country_dict(self):
+        id_ = "arrestWarrantCountryId"
+        countries = self.text_parsing().find(id=id_).find_all('option')
+        # print(countries)
+        for country in countries:
+            self.countries_dict[(str(country)).partition('"')[2].partition('"')[0]] = country.text
+        self.countries_dict.pop('')
+        print(f'Словарь стран создан-->{self.countries_dict}')
+        return
+
+    def param_maker(self):
+        latin_string = 'qwertyuiopasdfghjklzxcvbnm'
+        for symbol in latin_string:
+            param = {'name': symbol}
+            self.param_list.append(param)
+        return self.param_list
+
+    # создание папки для сохранения
+    def make_dir(self):
+        name_dir = "red_notice"
+        os.mkdir(name_dir)
+        for country in self.countries_dict.values():
+            dir_path = os.path.join(os.getcwd(), name_dir, country)
+            if not os.path.isdir(dir_path):
+                os.mkdir(dir_path)
+        print('Директории созданы')
+        return
+
+    # сохранение в соответствующую папку
+    def json_saving(self, list_, id_):
+        for red_notice in list_:
+            file_name = f'{red_notice["entity_id"].replace("/", "_")}.json'
+            directory = 'red_notice'
+            sub_dir = self.countries_dict[id_]
+            file_url = os.path.join(os.getcwd(), directory, sub_dir, file_name)
+            if not os.path.isfile(file_url):
+                with open(file_url, 'w') as f:
+                    json.dump(red_notice, f)
+                    self.super_count += 1
+
+    def make_json(self):
+        for country_id in self.countries_dict:
+            url = f"https://ws-public.interpol.int/notices/v1/red?&arrestWarrantCountryId={country_id}" \
+                   f"&resultPerPage=161"
+            response = requests.get(url, headers=self.headers)
+            if response.json()['total'] <= 160:
+                wanted_list = response.json()['_embedded']['notices']
+                # print(f'make_json- wanted_list-->{wanted_list}')
+                self.json_saving(wanted_list, country_id)
+            else:
+                self.big_wanted_list.append(response.json()['query']['arrestWarrantCountryId'])
+        # print(f'make_json --> {self.big_wanted_list}')
+        # for country in self.big_wanted_list:
+        #     self.big_json(country)
+        #
+
+        thread_list = []
+        for thr in self.big_wanted_list:
+            thr = threading.Thread(target=self.big_json, args=(thr,))
+            thr.start()
+            thread_list.append(thr)
+
+        for thr in thread_list:
+            thr.join()
+
+        print(f'Загрузка обьектов {self.super_count} шт. заверешена')
+        return
+
+
+    def big_json(self, country_id):
+        print(f'Загружаютя файлы из {self.countries_dict[country_id]}')
+        url = "https://ws-public.interpol.int/notices/v1/red"
+        headers = self.headers
+        params = {'ageMin': 0, 'ageMax': 120, 'arrestWarrantCountryId': country_id, 'resultPerPage': 160}
+        params_2 = {'sexId': 'U'}
+        response = requests.get(url, params=params_2 | params, headers=headers)
+        wanted_list = response.json()['_embedded']['notices']
+        self.json_saving(wanted_list, country_id)
+
+        for age in range(18, 100):
+            params_3 = {'sexId': 'F', 'ageMin': age, 'ageMax': age}
+            params_4 = {'sexId': 'M', 'ageMin': age, 'ageMax': age}
+            response = requests.get(url, params=params | params_3, headers=headers)
+            if response.json()['total'] > 160:
+                for param_name in self.param_list:
+                    response = requests.get(url, params=(params | params_3) | param_name, headers=headers)
+                    wanted_list = response.json()['_embedded']['notices']
+                    self.json_saving(wanted_list, country_id)
+            else:
+                wanted_list = response.json()['_embedded']['notices']
+                self.json_saving(wanted_list, country_id)
+
+            response = requests.get(url, params=params | params_4, headers=headers)
+            if response.json()['total'] > 160:
+                for param_name in self.param_list:
+                    response = requests.get(url, params=(params | params_4) | param_name, headers=headers)
+                    wanted_list = response.json()['_embedded']['notices']
+                    self.json_saving(wanted_list, country_id)
+            else:
+                wanted_list = response.json()['_embedded']['notices']
+                self.json_saving(wanted_list, country_id)
+
+
+            # url = f"https://ws-public.interpol.int/notices/v1/red?&arrestWarrantCountryId={country_id}" \
+            #       f"&resultPerPage=161"
+            #
+            # response = requests.get(url, headers=self.headers)
+            # if response.json()['total'] <= 160:
+            #     wanted_list = response.json()['_embedded']['notices']
+            #     # print(f'make_json- wanted_list-->{wanted_list}')
+            #     self.json_saving(wanted_list, country_id)
+
+        return self.super_param
+    # def big_json(self, country_id):
+    #     for age in range(18, 121):
+    #         url = "https://ws-public.interpol.int/notices/v1/red"
+    #         params = {'ageMin': age, 'ageMax': age, 'arrestWarrantCountryId': country_id, 'resultPerPage': 161}
+    #         headers = self.headers
+    #         wanted_list = requests.get(url, params=params, headers=headers).json()['_embedded']['notices']
+    #         self.json_saving(wanted_list, country_id)
+    #         print(wanted_list)
+class ParserYellow(ParserRed):
+    def get_total(self):
+        super().get_total().url = "https://ws-public.interpol.int/notices/v1/yellow"
+
+    def making_country_dict(self):
+        super().making_country_dict().id_ = 'nationality'
+
+    def make_dir(self):
+        super().make_dir().name_dir = 'yellow_notice'
+
+    def json_saving(self, list_, id_):
+        super().json_saving().directory = 'yellow_notice'
+
+    def make_json(self):
+        for country_id in self.countries_dict:
+            url = f"https://ws-public.interpol.int/notices/v1/yellow?&nationality={country_id}" \
+                  f"&resultPerPage=161"
+            response = requests.get(url, headers=self.headers)
+            if response.json()['total'] <= 160:
+                wanted_list = response.json()['_embedded']['notices']
+                # print(f'make_json- wanted_list-->{wanted_list}')
+                self.json_saving(wanted_list, country_id)
+            else:
+                self.big_wanted_list.append(response.json()['query']['nationality'])
+        # print(f'make_json --> {self.big_wanted_list}')
+
+        # for country in self.big_wanted_list:
+        # self.big_json(country)
+        AR = threading.Thread(target=self.big_json, args=('AR',))
+        AR.start()
+
+        SV = threading.Thread(target=self.big_json, args=('SV',))
+        SV.start()
+
+        IN = threading.Thread(target=self.big_json, args=('IN',))
+        IN.start()
+
+        PK = threading.Thread(target=self.big_json, args=('PK',))
+        PK.start()
+
+        RU = threading.Thread(target=self.big_json, args=('RU',))
+        RU.start()
+
+        US = threading.Thread(target=self.big_json, args=('US',))
+        US.start()
+
+        print(f'Загрузка обьектов {self.super_count} шт. заверешена')
+        return
+
+    def big_json(self, country_id):
+        print(f'Загружаютя файлы из {self.countries_dict[country_id]}')
+        url = "https://ws-public.interpol.int/notices/v1/yellow"
+        headers = self.headers
+        params = {'ageMin': 0, 'ageMax': 120, 'nationality': country_id, 'resultPerPage': 160}
+        params_2 = {'sexId': 'U'}
+        response = requests.get(url, params=params_2 | params, headers=headers)
+        wanted_list = response.json()['_embedded']['notices']
+        self.json_saving(wanted_list, country_id)
+
+        for age in range(0, 111):
+            params_3 = {'sexId': 'F', 'ageMin': age, 'ageMax': age}
+            params_4 = {'sexId': 'M', 'ageMin': age, 'ageMax': age}
+            response = requests.get(url, params=params | params_3, headers=headers)
+            if response.json()['total'] > 160:
+                for param_name in self.param_list:
+                    response = requests.get(url, params=(params | params_3) | param_name, headers=headers)
+                    wanted_list = response.json()['_embedded']['notices']
+                    self.json_saving(wanted_list, country_id)
+            else:
+                wanted_list = response.json()['_embedded']['notices']
+                self.json_saving(wanted_list, country_id)
+
+            response = requests.get(url, params=params | params_4, headers=headers)
+            if response.json()['total'] > 160:
+                for param_name in self.param_list:
+                    response = requests.get(url, params=(params | params_4) | param_name, headers=headers)
+                    wanted_list = response.json()['_embedded']['notices']
+                    self.json_saving(wanted_list, country_id)
+            else:
+                wanted_list = response.json()['_embedded']['notices']
+                self.json_saving(wanted_list, country_id)
+        return self.super_param
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from pprint import pprint
-import threading
 import time
-import os, shutil
-import requests, json
-import bs4
+import os
+import shutil
+
+from classes import ParserRed, ParserYellow
 
 url_1 = 'https://www.interpol.int/How-we-work/Notices/View-Red-Notices'
 url_2 = 'https://www.interpol.int/How-we-work/Notices/View-Yellow-Notices'
@@ -12,157 +11,17 @@ HEADERS = {
                       'Chrome/53.0.2785.143 Safari/537.36'
     }
 
-
-class Parser:
-    super_count = 0
-    countries_dict = {}
-    big_wanted_list = []
-    super_param = []
-    param_list = []
-
-    def __init__(self, url, headers):
-        self.url = url
-        self.headers = headers
-        self.get_total()
-        self.making_country_dict()
-        self.param_maker()
-        self.make_dir()
-        self.make_json()
-
-    def text_parsing(self):
-        response = requests.get(self.url, self.headers)
-        text = response.text
-        soup = bs4.BeautifulSoup(text, features="html.parser")
-        print('text_parsing')
-        return soup
-
-    def get_total(self):
-        response = requests.get("https://ws-public.interpol.int/notices/v1/red", headers=self.headers)
-        print(f'Необходимо загрузить {response.json()["total"]}')
-        return
-
-   # словарь со странами
-    def making_country_dict(self):
-        countries = self.text_parsing().find(id="arrestWarrantCountryId").find_all('option')
-        # print(countries)
-        for country in countries:
-            self.countries_dict[(str(country)).partition('"')[2].partition('"')[0]] = country.text
-        self.countries_dict.pop('')
-        print(f'Словарь стран создан-->{self.countries_dict}')
-        return
-
-    def param_maker(self):
-        latin_string = 'qwertyuiopasdfghjklzxcvbnm'
-        for symbol in latin_string:
-            param = {'name': symbol}
-            self.param_list.append(param)
-        return self.param_list
-# # словарь национальностей#
-#     def making_nationality_dict(self):
-#         nationalities = self.text_parsing().find(id="nationality").find_all('option')
-#         print(nationalities)
-#         #
-#         # nationalities_dict = {}
-#         # for nationality in nationalities:
-#         #     nationalities_dict[(str(nationality)).partition('"')[2].partition('"')[0]] = nationality.text
-#         # nationalities_dict.pop('')
-#         # return len(nationalities_dict)
-
-    # создание папки для сохранения
-    def make_dir(self):
-        os.mkdir("red_notice")
-        for country in self.countries_dict.values():
-            dir_path = os.path.join(os.getcwd(), 'red_notice', country)
-            if not os.path.isdir(dir_path):
-                os.mkdir(dir_path)
-        print('Директории созданы')
-        return
-
-    # сохранение в соответствующую папку
-    def json_saving(self, list_, id_):
-        for red_notice in list_:
-            file_name = f'{red_notice["entity_id"].replace("/", "_")}.json'
-            directory = 'red_notice'
-            sub_dir = self.countries_dict[id_]
-            file_url = os.path.join(os.getcwd(), directory, sub_dir, file_name)
-            if not os.path.isfile(file_url):
-                with open(file_url, 'w') as f:
-                    json.dump(red_notice, f)
-                    self.super_count += 1
-    def make_json(self):
-        for country_id in self.countries_dict:
-            url = f"https://ws-public.interpol.int/notices/v1/red?&arrestWarrantCountryId={country_id}" \
-                   f"&resultPerPage=161"
-            response = requests.get(url, headers=self.headers)
-            if response.json()['total'] <= 160:
-                wanted_list = response.json()['_embedded']['notices']
-                # print(f'make_json- wanted_list-->{wanted_list}')
-                self.json_saving(wanted_list, country_id)
-            else:
-                self.big_wanted_list.append(response.json()['query']['arrestWarrantCountryId'])
-        # print(f'make_json --> {self.big_wanted_list}')
-        for country in self.big_wanted_list:
-            self.big_json(country)
-        print(f'Загрузка обьектов {self.super_count} шт. заверешена')
-        return
-
-
-    def big_json(self, country_id):
-        print(f'Загружаютя файлы из {self.countries_dict[country_id]}')
-        url = "https://ws-public.interpol.int/notices/v1/red"
-        headers = self.headers
-        params = {'ageMin': 0, 'ageMax': 120, 'arrestWarrantCountryId': country_id, 'resultPerPage': 160}
-        params_2 = {'sexId': 'U'}
-        response = requests.get(url, params=params_2 | params, headers=headers)
-        wanted_list = response.json()['_embedded']['notices']
-        self.json_saving(wanted_list, country_id)
-
-        for age in range(18, 100):
-            params_3 = {'sexId': 'F', 'ageMin': age, 'ageMax': age}
-            params_4 = {'sexId': 'M', 'ageMin': age, 'ageMax': age}
-            response = requests.get(url, params=params | params_3, headers=headers)
-            if response.json()['total'] > 160:
-                for param_name in self.param_list:
-                    response = requests.get(url, params=(params | params_3) | param_name, headers=headers)
-                    wanted_list = response.json()['_embedded']['notices']
-                    self.json_saving(wanted_list, country_id)
-            else:
-                wanted_list = response.json()['_embedded']['notices']
-                self.json_saving(wanted_list, country_id)
-
-            response = requests.get(url, params=params | params_4, headers=headers)
-            if response.json()['total'] > 160:
-                for param_name in self.param_list:
-                    response = requests.get(url, params=(params | params_4) | param_name, headers=headers)
-                    wanted_list = response.json()['_embedded']['notices']
-                    self.json_saving(wanted_list, country_id)
-            else:
-                wanted_list = response.json()['_embedded']['notices']
-                self.json_saving(wanted_list, country_id)
-
-
-            # url = f"https://ws-public.interpol.int/notices/v1/red?&arrestWarrantCountryId={country_id}" \
-            #       f"&resultPerPage=161"
-            #
-            # response = requests.get(url, headers=self.headers)
-            # if response.json()['total'] <= 160:
-            #     wanted_list = response.json()['_embedded']['notices']
-            #     # print(f'make_json- wanted_list-->{wanted_list}')
-            #     self.json_saving(wanted_list, country_id)
-
-        return self.super_param
-    # def big_json(self, country_id):
-    #     for age in range(18, 121):
-    #         url = "https://ws-public.interpol.int/notices/v1/red"
-    #         params = {'ageMin': age, 'ageMax': age, 'arrestWarrantCountryId': country_id, 'resultPerPage': 161}
-    #         headers = self.headers
-    #         wanted_list = requests.get(url, params=params, headers=headers).json()['_embedded']['notices']
-    #         self.json_saving(wanted_list, country_id)
-    #         print(wanted_list)
 if __name__ == '__main__':
+
     start = time.time()
     if os.path.isdir('red_notice'):
         shutil.rmtree('red_notice')
-    a = Parser(url_1, HEADERS)
+    a = ParserRed(url_1, HEADERS)
     end = time.time()
-    print(f'Время загрузки {(end-start)/60}мин')
+    print(f'Время загрузки {round((end-start,2)/60)}мин')
+    # start = time.time()
+    # if os.path.isdir('yellow_notice'):
+    #     shutil.rmtree('yellow_notice')
+    # b = ParserYellow(url_2, HEADERS)
+    # end = time.time()
+    # print(f'Время загрузки {round(end-start,2)/60}мин')
